eed209bd268ba23d68a7f10024183afdec86f3b4
SALMA IMASSENDA <salmaimassenda2023@gmail.com>
2024-11-03 13:13:28 +0100

    tp Spring Data

---
+++ src/main/java/com/example/tpspringdataa/TpSpringDataaApplication.java
@@ -0,0 +1,13 @@
+package com.example.tpspringdataa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSpringDataaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpSpringDataaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tpspringdataa/TpSpringDataaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tpspringdataa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpSpringDataaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
